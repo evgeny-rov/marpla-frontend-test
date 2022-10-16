ccefb91b2d126eef92c9dccddb5dc6043eedde02
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-10-16 19:19:13 +0800

    fixed pagination and improved performance

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,15 +1,21 @@
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useState } from 'react';
 
-import { columns } from "./columns";
+import { columns } from './columns';
 
-export const DataTable = ({ rows }) => (
-  <DataGrid
-    style={{ minHeight: 590, width: "100%" }}
-    getRowId={(row) => row.Id}
-    rows={rows}
-    columns={columns}
-    pageSize={10}
-    rowsPerPageOptions={[10, 50, 100, 300]}
-    checkboxSelection
-  />
-);
+export const DataTable = React.memo(({ rows }) => {
+  const [pageSize, setPageSize] = useState(10);
+
+  return (
+    <DataGrid
+      style={{ minHeight: 590, width: '100%' }}
+      getRowId={(row) => row.Id}
+      rows={rows}
+      columns={columns}
+      pageSize={pageSize}
+      onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+      rowsPerPageOptions={[10, 50, 100, 300]}
+      checkboxSelection
+    />
+  );
+});
